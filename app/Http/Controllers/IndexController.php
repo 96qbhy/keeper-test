71a10a037b6aa9bc5d9e8f31feca21601a5ddd6e
96qbhy <96qbhy@gmail.com>
2018-03-05 18:11:11 +0800

    init

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Dybasedev\Keeper\Http\KeeperBaseController;
+use Dybasedev\Keeper\Http\Request;
+
+class IndexController extends KeeperBaseController
+{
+    public function index(Request $request)
+    {
+    
+    }
+}
